e41994f14d40955c3375b75b99dd600f15638b70
Olivier Koch <olivier@olivierkoch.org>
2016-07-25 22:26:22 +0200

    need to pack frame

--- nestor.py
+++ nestor.py
@@ -10,18 +10,19 @@ def Draw(root, bus_checker):
     """ draw function.  is called only once at startup. """
 
     # create the main frame
-    frame=tk.Frame(root,width=1800,height=500,relief='solid',bd=0,bg='white')
+    frame=tk.Frame(root,width=1000,height=500,relief='solid',bd=0,bg='white')
+    frame.pack()
 
     # select font
-    mfont = ("TkFixedFont",12)
-    mfont = ("-*-lucidatypewriter-medium-r-*-*-*-140-*-*-*-*-*-*",12)
-    mfont = ("FixedSys", 24)
 
     # create the graphical objects
     label_lines_names = []
     label_lines_times = []
     n_lines = len(bus_checker.lines_names_sorted())
     for v in bus_checker.lines_names_sorted():
+        mfont = ("TkFixedFont",12)
+        mfont = ("-*-lucidatypewriter-medium-r-*-*-*-140-*-*-*-*-*-*",12)
+        mfont = ("FixedSys", 24)
         label1 = tk.Label(frame,text=v.rjust(10),font=mfont)
         label2 = tk.Label(frame,text='',font=mfont)
         label1.config(bg='white',justify='right')
@@ -31,6 +32,7 @@ def Draw(root, bus_checker):
 
     # place objects on a grid
     for (label1,label2,k) in zip(label_lines_names,label_lines_times,range(n_lines)):
+        print('setting label %d' % k)
         label1.grid(row=k)
         label2.grid(row=k,column=1)
 
@@ -57,7 +59,7 @@ def Refresher(root, bus_checker, label_lines_times):
 
     # call itself
     root.after(1000, functools.partial(Refresher,root,bus_checker,label_lines_times)) # every second...
-    root.geometry('1000x500')
+    #root.geometry("500x500")
 
 
 def main():
