98b8fc715801a8e1d5cf7fef1bafcc8a43484af5
Olivier Koch <olivier@olivierkoch.org>
2016-07-25 21:55:33 +0200

    first commit

---
+++ nestor.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+from transport import BusChecker
+import Tkinter as tk
+import time
+import functools
+
+
+
+def Draw(root, bus_checker):
+    """ draw function.  is called only once at startup. """
+
+    # create the main frame
+    frame=tk.Frame(root,width=1800,height=500,relief='solid',bd=0,bg='white')
+
+    # select font
+    mfont = ("TkFixedFont",12)
+    mfont = ("-*-lucidatypewriter-medium-r-*-*-*-140-*-*-*-*-*-*",12)
+    mfont = ("FixedSys", 24)
+
+    # create the graphical objects
+    label_lines_names = []
+    label_lines_times = []
+    n_lines = len(bus_checker.lines_names_sorted())
+    for v in bus_checker.lines_names_sorted():
+        label1 = tk.Label(frame,text=v.rjust(10),font=mfont)
+        label2 = tk.Label(frame,text='',font=mfont)
+        label1.config(bg='white',justify='right')
+        label2.config(bg='white',justify='left')
+        label_lines_names.append(label1)
+        label_lines_times.append(label2)
+
+    # place objects on a grid
+    for (label1,label2,k) in zip(label_lines_names,label_lines_times,range(n_lines)):
+        label1.grid(row=k)
+        label2.grid(row=k,column=1)
+
+    return label_lines_times
+
+
+def Refresher(root, bus_checker, label_lines_times):
+    """ refresh function. is called repeatedly. """
+
+    print('refreshing at %s...' % time.asctime())
+    bus_checker.refresh()
+    times_sorted = bus_checker.lines_times_sorted()
+
+    # update text for each bus line
+    for v,k in zip(times_sorted, range(len(times_sorted))):
+        vt = ', '.join(['%d' % x for x in v])
+        if len(v) > 0:
+            vt += ' mn'
+        else:
+            vt = ''
+        vt = ' ' + vt.ljust(25)
+        print(vt)
+        label_lines_times[k].configure(text=vt)
+
+    # call itself
+    root.after(1000, functools.partial(Refresher,root,bus_checker,label_lines_times)) # every second...
+    root.geometry('1000x500')
+
+
+def main():
+    bus_checker = BusChecker()
+    bus_checker.refresh()
+
+    root = tk.Tk()
+    lines_next = Draw(root, bus_checker)
+    Refresher(root, bus_checker, lines_next)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
---
+++ transport.py
@@ -0,0 +1,56 @@
+import urllib2
+import re
+
+class BusChecker:
+
+    def __init__ (self):
+        self.urls = {\
+                'B_72':'http://www.ratp.fr/horaires/fr/ratp/bus/prochains_passages/PP/B72/72_94_107/A',\
+                'B_22':'http://www.ratp.fr/horaires/fr/ratp/bus/prochains_passages/PP/B22/22_31/A',\
+                'M_10':'http://www.ratp.fr/horaires/fr/ratp/metro/prochains_passages/PP/chardon+lagache/10/A',\
+                'M_9' :'http://www.ratp.fr/horaires/fr/ratp/metro/prochains_passages/PP/exelmans/9/A'}
+        self.re_search = re.compile ('[0-9]+ mn')
+        self.next_bus = {}
+
+
+    def to_string (self):
+        s = ''
+        for k,v in self.urls.iteritems():
+            s += '%s: ' % (k)
+            if k in self.next_bus:
+                for v in self.next_bus[k]:
+                    s += '%d ' % v
+            s += '\n'
+        return s
+
+
+    def __str__ (self):
+        s = ''
+        for k,v in self.urls.iteritems():
+            s += '%s: ' % (k)
+            if k in self.next_bus:
+                for v in self.next_bus[k]:
+                    s += '%d ' % v
+            s += '\n'
+        return s
+
+
+    def refresh (self):
+        self.next_bus = {}
+        for k,v in self.urls.iteritems():
+            response = urllib2.urlopen (v)
+            html = response.read()
+            times = re.findall (self.re_search,html)
+            self.next_bus[k] = []
+            for mtime in times:
+                stime = int(mtime.split(' ')[0])
+                self.next_bus[k].append (stime)
+
+
+    def lines_names_sorted (self):
+        return sorted(self.next_bus.keys())
+
+
+    def lines_times_sorted (self):
+        return [self.next_bus[x] for x in self.lines_names_sorted()]
+
