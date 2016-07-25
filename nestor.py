b7a02638c3b476408b26c70c2c126983d8ee6859
Olivier Koch <olivier@olivierkoch.org>
2016-07-25 22:39:40 +0200

    with time of day

--- nestor.py
+++ nestor.py
@@ -10,39 +10,50 @@ def Draw(root, bus_checker):
     """ draw function.  is called only once at startup. """
 
     # create the main frame
-    frame=tk.Frame(root,width=1000,height=500,relief='solid',bd=0,bg='white')
+    frame=tk.Frame(root,width=500,height=500,relief='solid',bd=0,bg='white')
     frame.pack()
 
     # select font
+    mfont = ("TkFixedFont",12)
+    mfont = ("-*-lucidatypewriter-medium-r-*-*-*-140-*-*-*-*-*-*",12)
+    mfont = ("FixedSys", 24)
 
     # create the graphical objects
     label_lines_names = []
     label_lines_times = []
     n_lines = len(bus_checker.lines_names_sorted())
     for v in bus_checker.lines_names_sorted():
-        mfont = ("TkFixedFont",12)
-        mfont = ("-*-lucidatypewriter-medium-r-*-*-*-140-*-*-*-*-*-*",12)
-        mfont = ("FixedSys", 24)
         label1 = tk.Label(frame,text=v.rjust(10),font=mfont)
         label2 = tk.Label(frame,text='',font=mfont)
         label1.config(bg='white',justify='right')
         label2.config(bg='white',justify='left')
         label_lines_names.append(label1)
         label_lines_times.append(label2)
+    label_time_of_day = tk.Label(frame,font=mfont)
+    label_time_of_day.config(bg='white',justify='right')
 
     # place objects on a grid
     for (label1,label2,k) in zip(label_lines_names,label_lines_times,range(n_lines)):
         print('setting label %d' % k)
         label1.grid(row=k)
         label2.grid(row=k,column=1)
+    label_time_of_day.grid(row=4,column=0)
 
-    return label_lines_times
+    objects = (label_time_of_day, label_lines_times)
+
+    return objects
 
 
-def Refresher(root, bus_checker, label_lines_times):
+def Refresher(root, bus_checker, objects):
     """ refresh function. is called repeatedly. """
 
     print('refreshing at %s...' % time.asctime())
+    
+    label_time_of_day = objects[0]
+    label_lines_times = objects[1]
+
+    label_time_of_day.configure(text=time.strftime('%A %d %Y %H:%M'))
+
     bus_checker.refresh()
     times_sorted = bus_checker.lines_times_sorted()
 
@@ -58,8 +69,8 @@ def Refresher(root, bus_checker, label_lines_times):
         label_lines_times[k].configure(text=vt)
 
     # call itself
-    root.after(1000, functools.partial(Refresher,root,bus_checker,label_lines_times)) # every second...
-    #root.geometry("500x500")
+    root.after(1000, functools.partial(Refresher,root,bus_checker,objects)) # every second...
+    root.geometry("500x500")
 
 
 def main():
@@ -67,8 +78,8 @@ def main():
     bus_checker.refresh()
 
     root = tk.Tk()
-    lines_next = Draw(root, bus_checker)
-    Refresher(root, bus_checker, lines_next)
+    objects = Draw(root, bus_checker)
+    Refresher(root, bus_checker, objects)
     root.mainloop()
 
 
